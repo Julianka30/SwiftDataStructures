b3bd60ce6c86f0091d722b396422d4dd3270c096
Julia <julia@Julias-MBP.N1>
2022-10-03 14:58:41 +0300

    Add empty project with Framework,Tests

---
+++ SwiftDataStructures/SwiftDataStructures/SwiftDataStructures.h
@@ -0,0 +1,18 @@
+//
+//  SwiftDataStructures.h
+//  SwiftDataStructures
+//
+//  Created by Julia on 9/23/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDataStructures.
+FOUNDATION_EXPORT double SwiftDataStructuresVersionNumber;
+
+//! Project version string for SwiftDataStructures.
+FOUNDATION_EXPORT const unsigned char SwiftDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDataStructures/PublicHeader.h>
+
+
